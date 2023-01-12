5222ce2a9f3e3e081b040b4707c5b71c66c81cb4
Topasqk <topasqksss@mail.ru>
2023-01-12 17:34:28 +0300

    new3 ArrayList & LinkedList

--- src/sorting/StackArrayList.java
+++ src/sorting/StackArrayList.java
@@ -1,12 +1,34 @@
 package sorting;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class StackArrayList {
+    public static void ArList(String[] array) {
+        List<String>arr1 = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+             arr1.add(array[i]);
+        }
+        System.out.println(arr1.toString());
+        System.out.println("Введите число ArrayList:");
+        Scanner scan1 = new Scanner(System.in);
+        String str = scan1.nextLine();
+        arr1.remove(str);
+        System.out.println(arr1.toString());
+    }
+    public static void LiList(String[] array){
+        List<String> arr2= new LinkedList<>();
+        for (int i = 0; i < array.length; i++) {
+            arr2.add(array[i]);
+        }
+        System.out.println(arr2.toString());
+        System.out.println("Введите число LinkedList:");
+        Scanner scan1 = new Scanner(System.in);
+        String str = scan1.nextLine();
+        arr2.remove(str);
+        System.out.println(arr2.toString());
+    }
     public static void main(String[] args) {
-
+        String[] array = new String[]{"zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         List<String> arr= new ArrayList<>();
         arr.add("zero"); //one two three four five six seven eight nine");
         arr.add("one");
@@ -19,11 +41,8 @@ public class StackArrayList {
         arr.add("eight");
         arr.add("nine");
         System.out.println(arr.toString());
-        System.out.println("Введите число:");
-        Scanner scan1 = new Scanner(System.in);
-        String str = scan1.nextLine();
-        arr.remove(str);
-        System.out.println(arr.toString());
+        ArList(array);
+        LiList(array);
 
 
     }
